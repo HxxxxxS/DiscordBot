952a5190dfab589cbb71f1e4d33d9265540133f6
HxxxxxS <HxxxxxS@users.noreply.github.com>
2017-04-22 19:45:16 +0200

    Added a stopedPlaying and error handler. Standardized indentation and quotation.

--- bot.js
+++ bot.js
@@ -1,28 +1,39 @@
-var config = require('./config.json');
+var config = require("./config.json");
 var currentSong, newSong;
 
 const http = require("http");
-const LastFmNode = require('lastfm').LastFmNode;
+const LastFmNode = require("lastfm").LastFmNode;
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 var lastfm = new LastFmNode({
-	api_key: config.api_key
+    api_key: config.api_key
 });
 
 var trackStream = lastfm.stream(config.username);
 
-trackStream.on('nowPlaying', function(track) {
-    newSong = track.artist["#text"] + ' - ' + track.name;
-    if (newSong != currentSong) {
-        console.info('currently playing:', newSong);
-        client.user.setGame(newSong);
-        currentSong = newSong;
+trackStream.on("nowPlaying", function(track) {
+    if(track){
+        newSong = track.artist["#text"] + " - " + track.name;
+        if (newSong != currentSong) {
+            console.info("currently playing:", newSong);
+            client.user.setGame(newSong);
+            currentSong = newSong;
+        }
     }
 });
 
-client.on('ready', () => {
+trackStream.on("stoppedPlaying", function(track) {
+    console.log("stopped playing.");
+    client.user.setGame(0);
+});
+
+trackStream.on("error", function(err) {
+    console.log("something went wrong: ",err);
+});
+
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}!`);
     trackStream.start();
 });
