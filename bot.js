70bef9a1f11f4dc3a19e04d5c0a2b0b528c34513
HxxxxxS <HxxxxxS@users.noreply.github.com>
2017-04-22 05:10:15 +0200

    forkety fork

--- bot.js
+++ bot.js
@@ -1,38 +1,30 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-var nodeSpotifyWebHelper = require('node-spotify-webhelper');
-var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
 var config = require('./config.json');
 var currentSong, newSong;
 
+const http = require("http");
+const LastFmNode = require('lastfm').LastFmNode;
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var lastfm = new LastFmNode({
+  api_key: config.api_key
+});
+
+var trackStream = lastfm.stream(config.username);
+
+trackStream.on('nowPlaying', function(track) {
+    newSong = track.artist["#text"] + ' - ' + track.name;
+    if (newSong != currentSong) {
+        console.info('currently playing:', newSong);
+        client.user.setGame(newSong);
+        currentSong = newSong;
+    }
+});
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.username}!`);
+    trackStream.start();
 });
 
-spotify.getStatus(function(err, res) { //Initially sets your game to the current song
-    if (err) {
-        return console.error(err);
-    }
-
-    console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
-
-    currentSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
-    client.user.setGame(currentSong);
-});
-
-setInterval(function(err, res) { //This Updates the song every 10 seconds
-    spotify.getStatus(function(err, res) {
-        if (err) {
-            return console.error(err);
-        }
-        newSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
-        if (newSong != currentSong) {
-          client.user.setGame(newSong);
-          console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
-          currentSong = newSong;
-        }
-    });
-}, 10000); //Change the value here to change how often the song is updated
-
-client.login(config.token)
+client.login(config.token);
