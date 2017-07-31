6b61cca75367851656a8e97993f4f880a83618cf
HxxxxxS <HxxxxxS@users.noreply.github.com>
2017-07-31 19:57:19 +0200

    make help command more readable

--- hxbot/Help.js
+++ hxbot/Help.js
@@ -7,12 +7,14 @@ var HelpModule = function () {
 HelpModule.prototype.Message = function(message)
 {
     var msg = "\n```Commands: \n";
+    msg += "-------------------------------------\n";
     for (var i = this.commands.length - 1; i >= 0; i--) {
         msg += config.commandPrefix+this.commands[i].cmd + ": " + this.commands[i].desc + "\n";
         if(this.commands[i].syntax)
         {
             msg += " "+"Example: "+config.commandPrefix+this.commands[i].syntax+"\n";
         }
+        msg += "-------------------------------------\n";
     }
     msg += "```";
     message.reply(msg);
