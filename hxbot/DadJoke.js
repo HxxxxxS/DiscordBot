d98245652d2272c6d91fc4158695cbe78dc71aed
HxxxxxS <16c52527@opayq.com>
2017-08-02 16:15:24 +0200

    Store joke ids in a db file to prevent duplicate jokes being told too often

--- hxbot/DadJoke.js
+++ hxbot/DadJoke.js
@@ -1,4 +1,7 @@
-var request = require('request');
+var request = require('request'),
+    JsonDB = require('node-json-db');
+
+var db = new JsonDB('used_jokes', true, false);
 
 var DadJokeModule = function () {};
 
@@ -7,27 +10,47 @@ DadJokeModule.prototype.Message = function(message)
 
     var request = require('request');
 
-    var headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'User-Agent': 'https://github.com/HxxxxxS/DiscordBot'
-    }
+    var jokes = db.getData('/jokes');
 
-    var options = {
-        url: 'https://icanhazdadjoke.com',
-        port: 443,
-        method: 'GET',
-        headers: headers
-    }
+    (function theThing()
+    {
 
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            message.reply(JSON.parse(body).joke);
-        }else{
-            console.log(error);
-            message.reply(`something went wrong. https://icanhazdadjoke.com/ returned HTTP status code: ${response.statusCode}`);
+        var headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'User-Agent': 'https://github.com/HxxxxxS/DiscordBot'
         }
-    })
+
+        var options = {
+            url: 'https://icanhazdadjoke.com',
+            port: 443,
+            method: 'GET',
+            headers: headers
+        }
+
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var joke = JSON.parse(body);
+                if(jokes.indexOf(joke.id) > -1)
+                {
+                    console.log(`we've already told joke ${joke.id}! rerolling`);
+                    theThing();
+                }else{
+                    message.reply(joke.joke);
+                    db.push('/jokes[]',joke.id);
+                    if(jokes.length>50)
+                    {
+                        db.delete('/jokes[0]');
+                    }
+                }
+            }else{
+                console.log((error?error:response));
+                message.reply(`something went wrong. https://icanhazdadjoke.com/ returned HTTP status code: ${response.statusCode}`);
+            }
+        });
+    }());
+
+    db.reload();
 
 }
 
