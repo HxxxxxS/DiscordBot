91a89567f4ba685603794c88c4182ae3fe792940
HxxxxxS <HxxxxxS@users.noreply.github.com>
2019-11-13 17:24:39 +0100

    removed references to a specific (dead) discord server

--- hxbot/Roll.js
+++ hxbot/Roll.js
@@ -16,25 +16,7 @@ RollModule.prototype.Message = function(message)
         var engine = Random.engines.mt19937().autoSeed();
         try {
             var n = Random.die(sideCount)(engine);
-            if(message.guild && message.guild.id == '340324466221514763')
-            {
-                if(n%10 == parseInt(n/10)%10){
-                    message.reply(`you rolled a <:checkem:342834590865686530> ${n}. Checkem!`);
-                }else{
-                        switch(n){
-                            case 420:
-                                message.reply(`you rolled a <:weed:341315349058289664> <:420:341290982534283264> <:weed:341315349058289664>`);
-                                break;
-                            case 69:
-                                message.reply(`you rolled a <:yaranaika:341315361666367488> 69.`);
-                                break;
-                            default:
-                                message.reply(`you rolled a ${n}.`);
-                        }
-                }
-            }else{
-                message.reply(`you rolled a ${n}.`);
-            }
+            message.reply(`you rolled a ${n}.`);
         } catch(error) {
             message.reply('error: Something went wrong. You probably picked a too high number.');
         };
