380f59e2b33cf2698122a3a7660db340a0ee459f
HxxxxxS <16c52527@opayq.com>
2019-12-22 16:27:13 +0100

    Changed restart.sh

--- restart.sh
+++ restart.sh
@@ -5,14 +5,18 @@ if [ "$(which forever 2>/dev/null)" == '' ]; then
     exit 1
 fi
 
-echo CRON: Cronjob started.
+echo "CRON: Cronjob started."
 
 forever stop discordbot
 
-echo CRON: Killed old forever process... Starting a new one.
+echo "CRON: Killed old forever process... Starting a new one."
 
 sleep 1
 
 forever --uid discordbot start -a index.js
 
-echo CRON: Bot is running. We are finished
+echo "CRON: Bot is running. We are finished"
+echo ""
+echo "Bot output: (ctrl+c will not kill the bot process)"
+
+tail -fn 0 ~/.forever/discordbot.log
