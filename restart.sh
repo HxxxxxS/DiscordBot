8a7974daacd47c2ca26e2875cc568b69b5f2a726
HxxxxxS <16c52527@opayq.com>
2019-12-22 16:00:07 +0100

    Added restart.sh script

---
+++ restart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$(which forever 2>/dev/null)" == '' ]; then
+    echo "This script requires https://github.com/foreversd/forever, please install and try again."
+    exit 1
+fi
+
+echo CRON: Cronjob started.
+
+forever stop discordbot
+
+echo CRON: Killed old forever process... Starting a new one.
+
+sleep 1
+
+forever --uid discordbot start -a index.js
+
+echo CRON: Bot is running. We are finished
