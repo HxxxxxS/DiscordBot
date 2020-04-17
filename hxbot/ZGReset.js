419bf5e9e1fb6fd05b5a9b4f23c4aad767e01008
HxxxxxS <16c52527@opayq.com>
2020-04-17 19:21:11 +0200

    Added ZG reset command

---
+++ hxbot/ZGReset.js
@@ -0,0 +1,159 @@
+var ZGModule = function () {};
+
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+ZGModule.prototype.Message = function(message)
+{
+    var reset = new Date('2020-04-13T08:00:00.000Z');
+
+    const rcd = 3;
+
+    const madnessCycleFull = ["Gri'lek", "Hazza'rah", "Renataki", "Wushoolay"];
+    const madnessCycle = [" Grilek ", " Hazza  ", "  Rena  ", " Wusho  "];
+    const madnessOriginWk = 16;
+    
+    const getMadnessBoss = (date) =>
+    {
+        let currWk = date.getWeek()
+        let tmpWk = madnessOriginWk;
+        let bossIndex = -1;
+        
+        while (tmpWk <= currWk)
+        {
+            bossIndex++;
+            if (bossIndex>madnessCycle.length) bossIndex = 0;
+            tmpWk++;
+        }
+        return madnessCycle[bossIndex];
+    }
+
+    const convertMilliseconds = (miliseconds, format) =>
+    {
+        let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
+
+        total_seconds = parseInt(Math.floor(miliseconds / 1000));
+        total_minutes = parseInt(Math.floor(total_seconds / 60));
+        total_hours = parseInt(Math.floor(total_minutes / 60));
+        days = parseInt(Math.floor(total_hours / 24));
+
+        seconds = parseInt(total_seconds % 60);
+        minutes = parseInt(total_minutes % 60);
+        hours = parseInt(total_hours % 24);
+
+        switch(format) {
+            case 's':
+                return seconds;
+            case 'm':
+                return minutes;
+            case 'h':
+                return hours;
+            case 'd':
+                return days;
+            default:
+                return { d: days, h: hours, m: minutes, s: seconds };
+        }
+    };
+
+    let currentDate = new Date();
+
+    while (currentDate > reset)
+    {
+        reset.setDate(reset.getDate() + rcd);
+    }
+
+    let resets = [];
+
+    for (var i =-1; i<9; i++)
+    {
+        var num = reset.getDate() + (rcd*i);
+        var mlength = new Date(reset.getYear(), reset.getMonth()+1, 0).getDate();
+        
+        if (num > mlength) num = num-mlength;
+
+        resets.push(num);
+    }
+
+    const renderCountdown = () => {
+        const seconds = convertMilliseconds(reset - currentDate, 's');
+        const minutes = convertMilliseconds(reset - currentDate, 'm');
+        const hours = convertMilliseconds(reset - currentDate, 'h');
+        const days = convertMilliseconds(reset - currentDate, 'd');
+        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+
+    const renderColumn = (index) => {
+        var col = "¯\n"
+        for (var i = 0 - reset.getDay() - 7; i < 14 - reset.getDay(); i++)
+        {
+            var day = new Date();
+            day.setMonth(reset.getMonth());
+            day.setDate(reset.getDate()+i+1);
+            
+            var text = (resets.indexOf(day.getDate()) > -1 ? 'RE' : day.getDate());
+            var paddedDay = ("`" + " ".repeat(4 - Math.max(text.toString().length),1) + text + " ".repeat(4 - Math.max(text.toString().length/2),1) + "` ");
+
+            if (day.getDate() == new Date().getDate()) paddedDay = "` today ` ";
+
+            if (index == 0)
+            {
+                if (day.getDay() == 1)
+                {
+                    col += "`"+getMadnessBoss(day)+"`";
+                    col += " | "+paddedDay+"\n\n";
+                }
+            } else if (index == 1)
+            {
+                if (day.getDay() == 2) col += paddedDay;
+                if (day.getDay() == 3) col += paddedDay;
+                if (day.getDay() == 4) col += paddedDay+" \n\n";
+            } else if (index == 2)
+            {
+                if (day.getDay() == 5) col += paddedDay;
+                if (day.getDay() == 6) col += paddedDay;
+                if (day.getDay() == 0) col += paddedDay+" \n\n";
+            }
+        }
+        return col;
+    }
+
+    message.channel.send(
+    `Next Zul\'Gurub reset is in **${renderCountdown()}**`,{
+        embed: {
+            fields: [{
+                name:'Upcoming resets and kill windows:',
+                value: "-",
+                inline: false
+             },
+             {
+                name: "`  EoM   ` | `  Mon  `",
+                value: renderColumn(0),
+                inline: true
+             },
+             {
+                name: "`  Tue  ` `  Wed  ` `  Thu   `",
+                value: renderColumn(1),
+                inline: true
+             },
+             {
+                name: "`  Fri  ` `  Sat  ` `  Sun  `",
+                value: renderColumn(2),
+                inline: true
+             }],
+             footer: {
+                text:`Every reset is at 09:00 AM server time\nMolten Core and Blackwing Lair always resets on Wednesday morning.`
+             }
+         }
+     })
+ }
+
+module.exports = ZGModule;
