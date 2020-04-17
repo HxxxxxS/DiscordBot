90924f9d168b3f6fc7b60e8261b0629da7537dce
HxxxxxS <16c52527@opayq.com>
2020-04-17 21:07:45 +0200

    Made the onyxia command rich embed like the ZG one

--- hxbot/OnyxiaReset.js
+++ hxbot/OnyxiaReset.js
@@ -4,6 +4,8 @@ OnyxiaModule.prototype.Message = function(message)
 {
     var reset = new Date('2019-12-30T07:00:00.000Z');
 
+    const rcd = 5;
+
     const convertMilliseconds = (miliseconds, format) =>
     {
         let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
@@ -35,7 +37,19 @@ OnyxiaModule.prototype.Message = function(message)
 
     while (currentDate > reset)
     {
-        reset.setDate(reset.getDate() + 5);
+        reset.setDate(reset.getDate() + rcd);
+    }
+
+    let resets = [];
+
+    for (var i =-1; i<5; i++)
+    {
+        var num = reset.getDate() + (rcd*i);
+        var mlength = new Date(currentDate.getYear(), currentDate.getMonth() + 1, 0).getDate();
+
+        if (num > mlength) num = num-mlength;
+
+        resets.push(num);
     }
 
     const renderCountdown = () => {
@@ -55,13 +69,75 @@ OnyxiaModule.prototype.Message = function(message)
 
             if (day.getDay() == 1) cal += "\n";
 
-            var text = ([reset.getDate(), reset.getDate() + 5, reset.getDate() + 10, reset.getDate() + 15].indexOf(day.getDate()) > -1 ? 'RE' : day.getDate());
+            var text = (resets.indexOf(day.getDate()) > -1 ? 'RE' : day.getDate());
             cal+= (" ".repeat(2 - text.toString().length)) + text + (day.getDay()>0 ? "     " : "\n");
         }
         return cal;
     }
+    
+    const renderColumn = (index) => {
+        var col = "¯\n"
+        for (var i = 0 - reset.getDay(); i < 21 - reset.getDay(); i++)
+        {
+            var day = new Date();
+            day.setMonth(reset.getMonth());
+            day.setDate(reset.getDate()+i+1);
+            
+            var text = (resets.indexOf(day.getDate()) > -1 ? 'RE' : day.getDate());
+            var paddedDay = ("`" + " ".repeat(4 - Math.max(text.toString().length),1) + text + " ".repeat(4 - Math.max(text.toString().length/2),1) + "` ");
 
-    message.channel.send(`Next Onyxia reset is in **${renderCountdown()}**\nUpcoming resets and kill windows:\n\`\`\`${renderCalendar()}\`\`\`*(Every reset is at 07:00 AM server time)*\nMolten Core and Blackwing Lair always resets on Wednesday morning.`)
-}
+            if (day.getDate() == new Date().getDate()) paddedDay = "` today ` ";
+            
+            if (index == 0)
+            {
+                if (day.getDay() == 1)
+                {
+                    col += "`        `";
+                    col += " | "+paddedDay+"\n\n";
+                }
+            } else if (index == 1)
+            {
+                if (day.getDay() == 2) col += paddedDay;
+                if (day.getDay() == 3) col += paddedDay;
+                if (day.getDay() == 4) col += paddedDay+" \n\n";
+            } else if (index == 2)
+            {
+                if (day.getDay() == 5) col += paddedDay;
+                if (day.getDay() == 6) col += paddedDay;
+                if (day.getDay() == 0) col += paddedDay+" \n\n";
+            }
+        }
+        return col;
+    }
+    
+    message.channel.send(
+    `Next Onyxia reset is in **${renderCountdown()}**`,{
+        embed: {
+            fields: [{
+                name:'Upcoming resets and kill windows:',
+                value: "-",
+                inline: false
+             },
+             {
+                name: "`        ` | `  Mon  `",
+                value: renderColumn(0),
+                inline: true
+             },
+             {
+                name: "`  Tue  ` `  Wed  ` `  Thu   `",
+                value: renderColumn(1),
+                inline: true
+             },
+             {
+                name: "`  Fri  ` `  Sat  ` `  Sun  `",
+                value: renderColumn(2),
+                inline: true
+             }],
+             footer: {
+                text:`Every reset is at 08:00 AM server time\nMolten Core and Blackwing Lair always resets on Wednesday morning.`
+             }
+         }
+     })
+ }
 
 module.exports = OnyxiaModule;
