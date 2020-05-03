a80f8c45535afca95a6c44bca622b0c8b15c8f19
HxxxxxS <HxxxxxS@users.noreply.github.com>
2020-05-03 23:06:59 +0200

    oops forgot we need this + untested

--- hxbot/OnyxiaReset.js
+++ hxbot/OnyxiaReset.js
@@ -10,6 +10,64 @@ const pad = (number, length) => {
 
 OnyxiaModule.prototype.Message = function(message)
 {
+    var reset = new Date('2019-12-30T07:00:00.000Z');
+
+    const rcd = 5;
+
+    const convertMilliseconds = (miliseconds, format) =>
+    {
+        let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
+
+        total_seconds = parseInt(Math.floor(miliseconds / 1000));
+        total_minutes = parseInt(Math.floor(total_seconds / 60));
+        total_hours = parseInt(Math.floor(total_minutes / 60));
+        days = parseInt(Math.floor(total_hours / 24));
+
+        seconds = parseInt(total_seconds % 60);
+        minutes = parseInt(total_minutes % 60);
+        hours = parseInt(total_hours % 24);
+
+        switch(format) {
+            case 's':
+                return seconds;
+            case 'm':
+                return minutes;
+            case 'h':
+                return hours;
+            case 'd':
+                return days;
+            default:
+                return { d: days, h: hours, m: minutes, s: seconds };
+        }
+    };
+
+    let currentDate = new Date();
+
+    while (currentDate > reset)
+    {
+        reset.setDate(reset.getDate() + rcd);
+    }
+
+    let resets = [];
+
+    for (var i =-1; i<5; i++)
+    {
+        var num = reset.getDate() + (rcd*i);
+        var mlength = new Date(currentDate.getYear(), currentDate.getMonth() + 1, 0).getDate();
+
+        if (num > mlength) num = num-mlength;
+
+        resets.push(num);
+    }
+
+    const renderCountdown = () => {
+        const seconds = convertMilliseconds(reset - currentDate, 's');
+        const minutes = convertMilliseconds(reset - currentDate, 'm');
+        const hours = convertMilliseconds(reset - currentDate, 'h');
+        const days = convertMilliseconds(reset - currentDate, 'd');
+        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+
     message.channel.send(
     `Next Onyxia reset is in **${renderCountdown()}**`,{
         embed: {
