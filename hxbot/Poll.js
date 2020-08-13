0f7c29f4192a069c41b79d185736e57ed7681f99
HxxxxxS <16c52527@opayq.com>
2020-08-14 00:10:07 +0200

    Fix poll module so it just adds every emoji in a message as an option, print message as seen

--- hxbot/Poll.js
+++ hxbot/Poll.js
@@ -9,12 +9,10 @@ PollModule.prototype.Message = function(message)
     }
     var keyword = 'poll';
     var pollIndex = message.content.indexOf(keyword);
-    var optionIndex = message.content.replace(/[<>\W]+/g, ' ').length;
 
-    var ret = `**${message.author.username} asked:**\n`;
-    var options = message.content.substring(optionIndex).trim().split(' ').map(s => s.replace(/[<>]/g, ''));
-
-    var question = message.content.substring(pollIndex + keyword.length, optionIndex).trim();
+    var ret = `**${message.member.displayName} asked:**\n`;
+    var options = message.content.trim().match(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g);
+    var question = message.content.substring(pollIndex + keyword.length).trim();
 
     ret += question;
 
